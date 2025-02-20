dcae0b72c8d684064e9581c1970cc1213d6e2684
Rownak Abdullah <rownakabdullahomi@gmail.com>
2025-02-20 12:48:49 +0600

    added firebase config again

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,13 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB15Z_aB6PG6tADUYjL2ip2CYR62aps1eY",
-  authDomain: "task-management-ae3a9.firebaseapp.com",
-  projectId: "task-management-ae3a9",
-  storageBucket: "task-management-ae3a9.firebasestorage.app",
-  messagingSenderId: "472550240672",
-  appId: "1:472550240672:web:d2ad32798bf124205eb4c5",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
